016ff3bdc5fcc6131f2c4e43aa78ef0ff82ae210
Yoann <yoanncancalon@gmail.com>
2024-01-28 20:46:51 +0100

    commit

---
+++ src/Controller/AnnuaireController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnuaireController extends AbstractController
+{
+    #[Route('/', name: 'app_annuaire')]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        $contacts = $contactRepository->findall();
+        
+        return $this->render('annuaire/index.html.twig', [
+            'contacts' => $contacts,
+        ]);
+    }
+
+}
